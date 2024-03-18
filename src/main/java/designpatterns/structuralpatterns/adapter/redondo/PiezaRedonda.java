d549e7f5825ca871d3555ffc4f489bd0604ac1aa
Sebastian Abello <juanse.abello.2004@gmail.com>
2024-03-17 19:28:49 -0500

    ✅ Adapter

---
+++ src/main/java/designpatterns/structuralpatterns/adapter/redondo/PiezaRedonda.java
@@ -0,0 +1,16 @@
+package designpatterns.structuralpatterns.adapter.redondo;
+
+public class PiezaRedonda {
+    private double radio;
+
+    public PiezaRedonda() {
+    }
+
+    public PiezaRedonda(double radio) {
+        this.radio = radio;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+}
